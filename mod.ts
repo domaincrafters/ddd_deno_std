5483ef4f05eb01179b763d3f6c914fa16b336bd7
Matthias Blomme <matthias.blomme@howest.be>
2024-10-15 13:50:35 +0200

    docs: added module documentation to the package mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2024 Matthias Blomme and Dimitri Casier
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+/**
+ * @module DomainCrafters Standard Utilities
+ *
+ * This module serves as a centralized collection of standard utility functions and types
+ * provided by the DomainCrafters library. It re-exports several essential components
+ * from the `@domaincrafters/std` package, making them easily accessible for developers.
+ */
 export * from "@domaincrafters/std/Optional.ts";
 export * from "@domaincrafters/std/Guard.ts";
 export * from "@domaincrafters/std/Exception.ts";
